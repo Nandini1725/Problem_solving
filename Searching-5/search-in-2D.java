ded756b6a24e4654e5ffdf16200c9d74deefd4b2
Nandini1725 <115562198+Nandini1725@users.noreply.github.com>
2024-03-19 16:29:46 +0530

    Update search-in-2D.java

--- Searching-5/search-in-2D.java
+++ Searching-5/search-in-2D.java
@@ -21,4 +21,29 @@ class Solution {
     }
 }
 
-// binary search
+// binary search approach
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int low = 0;
+        int high = (n*m)-1;
+
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            int i = mid/m;
+            int j = mid%m;
+            if(matrix[i][j]==target)
+            return true;
+
+            else if(matrix[i][j]>target)
+            high = mid-1;
+
+            else 
+            low=mid+1;
+        }
+        return false;
+    }
+}
