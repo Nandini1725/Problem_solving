319e88777faf4611cfe09164169adf84759e5dda
Nandini1725 <115562198+Nandini1725@users.noreply.github.com>
2024-05-26 23:25:46 +0530

    Create length-of-the-longest-subsequence-that-sums-to-target.java

---
+++ Dynamic Programming 4/length-of-the-longest-subsequence-that-sums-to-target.java
@@ -0,0 +1,27 @@
+class Solution {
+    private int solve(List<Integer> nums, int i, int target, Integer[][] dp) {
+        if(target == 0) {
+            return 1;
+        }
+        if(target < 0 || i < 0) {
+            return 0;
+        }
+        
+        if(dp[i][target] != null) {
+            return dp[i][target];
+        }
+        
+        int take = solve(nums, i-1, target-nums.get(i), dp);
+        int notTake = solve(nums, i-1, target, dp);
+        int res = 0;
+        if(take == 0 && notTake == 0) {
+            res = 0;
+        } else {
+            res = Math.max(take+1, notTake);
+        }
+        return dp[i][target] = res;
+    }
+    public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
+        return solve(nums, nums.size()-1, target, new Integer[nums.size()][target+1])-1;
+    }
+}
